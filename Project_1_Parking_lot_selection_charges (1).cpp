6b1b7a07aa665fd6d058132bfc6ac557238a0fd1
leftyluci <kyuuta@outlook.com>
2017-09-14 14:53:21 -0400

    Added Project Shell

---
+++ Project_1_Parking_lot_selection_charges (1).cpp
@@ -0,0 +1,26 @@
+/* Name : Keymer Botero , Zailuj Hernandez	*
+   Date : September 14th, 2017				*
+   Due Date : September 25th, 2017			*
+   Program Name : Project 1 - Passaic County*
+   					Parking Authority		*
+   Course # : CIS-161-ME1					*
+   Program Description : This program allows*
+   					the user to input data  *
+   					consisting of vehicle	*
+   					type and 4 integers		*
+   					representing entering	*
+   					and leaving hours and	*
+   					minutes in a			*
+   					user-friendly manner.	*
+*********************************************/
+
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+
+using namespace std;
+
+main()
+{
+	
+}
