ddf489aaf59ce8f16a0475ef022db59b41bad77e
Hot-Drinks <31969143+Hot-Drinks@users.noreply.github.com>
2017-09-14 15:02:44 -0400

    Indented asterisks
    For neatness

--- Project_1_Parking_lot_selection_charges.cpp
+++ Project_1_Parking_lot_selection_charges.cpp
@@ -1,18 +1,19 @@
-/* Name : Keymer Botero , Zailuj Hernandez	*
-   Date : September 14th, 2017				*
-   Due Date : September 25th, 2017			*
-   Program Name : Project 1 - Passaic County*
-   					Parking Authority		*
+/* Name : Keymer Botero , Zailuj Hernandez			*
+   Date : September 14th, 2017					*
+   Due Date : September 25th, 2017				*
+   Program Name : Project 1 - Passaic County			*
+   					Parking Authority	*
    Course # : CIS-161-ME1					*
-   Program Description : This program allows*
+   Program Description : This program allows			*
    					the user to input data  *
    					consisting of vehicle	*
-   					type and 4 integers		*
+   					type and 4 integers	*
    					representing entering	*
    					and leaving hours and	*
-   					minutes in a			*
+   					minutes in a		*
    					user-friendly manner.	*
-*********************************************/
+*****************************************************************
+*/
 
 #include <iostream>
 #include <math.h>
